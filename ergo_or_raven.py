b8d109556e1e26fc298136a9f8af45823dc687ab
LarsFOMO <90476796+LarsFOMO@users.noreply.github.com>
2022-09-20 09:46:47 +0200

    Add files via upload

--- ergo_or_raven.py
+++ ergo_or_raven.py
@@ -1,7 +1,16 @@
 from profitability import Profitability
+import requests
+
+urlERGO = "https://min-api.cryptocompare.com/data/blockchain/mining/calculator?fsyms=ERG&tsyms=USD"
 
 ergo = Profitability('ERG','USD', 130, 220)
 raven = Profitability('RVN','USD', 22, 220)
 
 print(ergo.get_price())
 print(raven.get_network_hashrate())
+
+print(ergo.get_block_reward())
+data = requests.get(urlERGO)
+dataOut = data.json()
+
+print(dataOut)
--- profitability.py
+++ profitability.py
@@ -18,12 +18,20 @@ urlRAVEN = "https://min-api.cryptocompare.com/data/blockchain/mining/calculator?
 class Profitability:
 
     def __init__(self, name, currency1, hashrate, powerUse):
+
         self.name = name
         self.currency1 = currency1
         self.hashrate = hashrate
         self.powerUse = powerUse
+        url1 = "https://min-api.cryptocompare.com/data/blockchain/mining/calculator?fsyms="
+        url2 = "&tsyms="
+        self.fullURL = url1 + self.name + url2 + self.currency1
+        data = requests.get(self.fullURL)
+        dataOut = data.json()
+        self.oo = [int(x) for x in re.findall(r' \d+', str(dataOut))]
     
     def get_price(self):
+
         self.price = cryptocompare.get_historical_price(self.name,self.currency1)      #130MH/s
         string1 = str(self.price)
         num1 = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", string1)
@@ -31,12 +39,14 @@ class Profitability:
         return self.price
 
     def get_network_hashrate(self):
-        url1 = "https://min-api.cryptocompare.com/data/blockchain/mining/calculator?fsyms="
-        url2 = "&tsyms="
-        fullURL = url1 + self.name + url2 + self.currency1
-        data = requests.get(fullURL)
-        dataOut = data.json()
-        oo = [int(x) for x in re.findall(r' \d+', str(dataOut))]
-        network_hashrate = oo[1]
+
+        network_hashrate = self.oo[1]
         return network_hashrate
+
+    def get_block_reward(self):
+
+        block_reward = self.oo[4]
+        return(block_reward)
+        
+
 
