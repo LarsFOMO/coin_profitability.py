75e3f90e1874920c12a93f2e006c21b80eb1750e
LarsFOMO <90476796+LarsFOMO@users.noreply.github.com>
2022-09-20 11:29:24 +0200

    Add files via upload

--- ergo_or_raven.py
+++ ergo_or_raven.py
@@ -12,11 +12,14 @@ power_consumption_raven = 220
 ergo = Profitability('ERG','USD', hashrate_ergo, power_consumption_ergo)
 raven = Profitability('RVN','USD', hashrate_raven, power_consumption_raven)
 
-print(ergo.get_price())
-print(ergo.get_network_hashrate())
-print(ergo.get_block_reward())
-print(ergo.get_mined_blocks_per_day())
-print(ergo.get_hashrate_share())
-print(ergo.get_daily_block_reward())
-print(ergo.get_daily_mining_income())
-print(raven.get_daily_mining_income())
+print("\nPrice [ERG]: ", ergo.get_price())
+print("Network Hashrate: ", ergo.get_network_hashrate())
+print("Block Reward: ",ergo.get_block_reward())
+print("Mined Blocks/Day: ","%.2f" % ergo.get_mined_blocks_per_day())
+print("Hashrate Share: ", ergo.get_hashrate_share())
+print("Daily Block Reward: ", "%.2f" % ergo.get_daily_block_reward())
+
+if(ergo.get_daily_mining_income() >= raven.get_daily_mining_income()):
+    print("\nErgo (","%.4f" % ergo.get_daily_mining_income(), "USD/Tag) ist aktuell profitabler, zu minen, als Raven (","%.4f" % raven.get_daily_mining_income(), "USD/Tag)\n")
+elif(ergo.get_daily_mining_income() < raven.get_daily_mining_income()):
+    print("\nRaven (","%.4f" % raven.get_daily_mining_income(), "USD/Tag) ist aktuell profitabler, zu minen, als Ergo (","%.4f" % ergo.get_daily_mining_income(), "USD/Tag)\n")
